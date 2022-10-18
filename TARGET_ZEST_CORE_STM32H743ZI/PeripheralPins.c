fd634393461c0ead7b97b83247acce3b747547da
Clément Laigle <c.laigle@catie.fr>
2022-10-18 16:16:15 +0200

    Initial commit

---
+++ TARGET_ZEST_CORE_STM32H743ZI/PeripheralPins.c
@@ -0,0 +1,4 @@
+/* 
+ * PeripheralPins.c describes the available pins and their association with peripherals.
+ * See: https://os.mbed.com/docs/mbed-os/latest/porting/index.html
+ */
---
+++ TARGET_ZEST_CORE_STM32H743ZI/PinNames.h
@@ -0,0 +1,4 @@
+/*
+ * PinNames.h sets macros for pins that define their function.
+ * See: https://os.mbed.com/docs/mbed-os/latest/porting/index.html
+ */
